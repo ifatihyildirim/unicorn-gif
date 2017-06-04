c2ab08e41525711a4e68ccf302713b81d27c6dc9
faaatihyildirim <sfthldrm@gmail.com>
2017-06-04 19:16:52 +0300

    done

---
+++ src/components/Preview.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+    <li v-for="gif in gifs" >
+      <img v-bind:src="gif.images.fixed_height.url"  />
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['gifs'],
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+ul{
+  
+}
+
+li{
+  list-style: none;
+  float: left;
+  margin: 20px;
+}
+</style>
